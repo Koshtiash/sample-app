197a16d5c6fe2d0dd664b3de69b7be5c89c268fa
Koshtiash <student-02-4bb80e8db3ad@qwiklabs.net>
2026-08-18 21:22:24 +0000

    Fix main.go and deploy dev v2.0

--- main.go
+++ main.go
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package main
 
 import (
@@ -32,12 +31,22 @@ func main() {
 
 func blueHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
+	draw.Draw(img, img.Bounds(),
+		&image.Uniform{color.RGBA{0, 0, 255, 255}},
+		image.ZP,
+		draw.Src)
+
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
 }
 
 func redHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
-	w.Header().Set("Content-Type", "image
+	draw.Draw(img, img.Bounds(),
+		&image.Uniform{color.RGBA{255, 0, 0, 255}},
+		image.ZP,
+		draw.Src)
+
+	w.Header().Set("Content-Type", "image/png")
+	png.Encode(w, img)
+}
